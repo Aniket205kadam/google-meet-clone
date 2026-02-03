ad0f015d07f4a3402c631078e5bc7713b63ecffd
Aniket205kadam <aniketrkadam205@gmail.com>
2026-02-03 16:31:58 +0530

    Fix: #3 Continue button not working after selecting contact, blocking flow

--- client/src/pages/home/HomeDesktop.jsx
+++ client/src/pages/home/HomeDesktop.jsx
@@ -57,6 +57,7 @@ const HomeDesktop = ({
   const [isShowCreateMeetingCode, setIsShowCreateMeetingCode] = useState(false);
   const [customToast, setCustomToast] = useState({ status: false, info: "" });
   const [isShowAccountInfo, setIsShowAccountInfo] = useState(false);
+  const [isContinuesBtnLoading, setIsContinuesBtnLoading] = useState(false);
 
   const userService = new UserService(accessToken);
   const meetingService = new MeetingService(accessToken);
@@ -80,10 +81,10 @@ const HomeDesktop = ({
   useClickOutside(callMoreRef, () => setIsShowCallMore(false));
   useClickOutside(searchMoreContainerRef, () => setIsShowSearch(false));
   useClickOutside(meetingOptionRef, () =>
-    setIsShowMeetingOption({ status: false, clientX: 0, clientY: 0 })
+    setIsShowMeetingOption({ status: false, clientX: 0, clientY: 0 }),
   );
   useClickOutside(createMeetingCodeRef, () =>
-    setIsShowCreateMeetingCode(false)
+    setIsShowCreateMeetingCode(false),
   );
   useClickOutside(accountInfoRef, () => setIsShowAccountInfo(false));
 
@@ -577,7 +578,7 @@ const HomeDesktop = ({
                             }
                             if (selectedUser !== null) {
                               toast.info(
-                                "Selecting multiple users is not supported at the moment."
+                                "Selecting multiple users is not supported at the moment.",
                               );
                             }
                             setSelectedUser(user);
@@ -625,7 +626,7 @@ const HomeDesktop = ({
                             }
                             if (selectedUser !== null) {
                               toast.info(
-                                "Selecting multiple users is not supported at the moment."
+                                "Selecting multiple users is not supported at the moment.",
                               );
                             }
                             setSelectedUser(user);
@@ -658,17 +659,37 @@ const HomeDesktop = ({
                   <div className="searched-continued-container">
                     <button
                       className={`${selectedUser != null ? "btn-active" : ""}`}
+                      onClick={() => {
+                        setIsContinuesBtnLoading(true);
+                        // delay
+                        setTimeout(() => {
+                          setIsContinuesBtnLoading(false);
+                          navigate(`/before-call/${selectedUser.id}`);
+                        }, 2000);
+                      }}
                     >
                       Continue{" "}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="#e3e3e3"
-                      >
-                        <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
-                      </svg>
+                      {isContinuesBtnLoading ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="24px"
+                          viewBox="0 -960 960 960"
+                          width="24px"
+                          fill="#e3e3e3"
+                        >
+                          <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q17 0 28.5 11.5T520-840q0 17-11.5 28.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-17 11.5-28.5T840-520q17 0 28.5 11.5T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="24px"
+                          viewBox="0 -960 960 960"
+                          width="24px"
+                          fill="#e3e3e3"
+                        >
+                          <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+                        </svg>
+                      )}
                     </button>
                   </div>
                 </div>
